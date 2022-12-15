1f80f13541aa5d3c62c94f6342874573800671f2
Kenneth Chang <kennethchang@kenneths-air.lan>
2022-12-15 09:55:03 -0800

    SortJunkie lecture complete

--- sortingTest.js
+++ sortingTest.js
@@ -1,39 +1,15 @@
-function merge(arr1, arr2) {
-  const results = [];
-  let i = 0;
-  let j = 0;
-  while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] < arr2[j]) {
-      results.push(arr1[i])
-      i++
-    } else {
-      results.push(arr2[j]);
-      j++
-    }
-  }
-  while (i < arr1.length) {
-    results.push(arr1[i])
-    i++
-  }
-  while (j < arr2.length) {
-    results.push(arr2[j])
-    j++
-  }
-  return results;
-}
+let instructors = [
+  {name: "Sarah Rose", favLang: "Go"},
+  {name: "Troy", favLang: "R"},
+  {name: "Ken", favLang: "English"}
+]
 
-// WINNER WINNER CHICKEN DINNER
-// console.log(merge([5, 8, 8, 8, 8, 20], [1, 3, 6, 7, 7, 7, 7, 7]))
+instructors.sort() // not gonna help
 
+const test = instructors.sort( (a, b) => {
+    if (a.name < b.name) return -1
+    if (a.name > b.name) return 1
+    return 0;
+})
 
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr 
-  }
-  const mid = Math.floor(arr.length / 2)
-  const left = mergeSort(arr.slice(0, mid)) 
-  const right = mergeSort(arr.slice(mid))
-  return merge(left, right);
-}
-
-console.log(mergeSort([43, 232, 24, 42, 5, 43, 3, 2, 33, 4, 32, 34, 366, 97, -4, -77]))
+console.log(test);
